2ec68868af100e79cc3130092f8b2191cf77d611
danm364 <danm364@yahoo.com>
2022-07-19 21:53:42 -0400

    Signup alert

--- login.js
+++ login.js
@@ -1,42 +1,46 @@
 
 
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+//const loginForm = document.getElementById("login-form");
+//const loginButton = document.getElementById("login-form-submit");
+//const loginErrorMsg = document.getElementById("login-error-msg");
 
+//console.log(loginForm);
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+//loginButton.addEventListener("click", (e) => {
+  //  e.preventDefault();
+   // const username = loginForm.username.value;
+    //const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+   // if (username === "user" && password === "web_dev") {
+    //    alert("You have successfully logged in.");
+     //   location.reload();
+    //} else {
+     //   loginErrorMsg.style.opacity = 1;
+   // }
+//})
 
 //signup details
-var signupUser = document.getElementById("user-sign-up")
-var signupPassword = document.getElementById("password-field")
-var passwordConfirm = document.getElementById("password-confirm-field")
-var signupButton = document.getElementById("user-sign-up")
+const UserForm = document.getElementById('signup-form');
+const signupButton = document.getElementById('user-sign-up');
+const passwordConfirmation = document.getElementById('password-confirm-field').value;
+const passwordNewUser = document.getElementById('password-field').value
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    function createParagraph() {
-      const para = document.createElement('p');
-      para.textContent = 'You clicked the button!';
-      document.body.appendChild(para);
-    }
-  
-    const buttons = document.querySelectorAll('button');
-  
-    for (const button of buttons) {
-      button.addEventListener('click', createParagraph);
-    }
-  });
+function signUpAlert() {
+  const passwordConfirmation = document.getElementById('password-confirm-field').value;
+  const passwordNewUser = document.getElementById('password-field').value
+  const signupUserName = document.getElementById('username-field').value
+  if (passwordConfirmation === passwordNewUser && signupUserName.length > 8 ){
+    alert('You are now signed up');
+    location.reload();
+}
+
+
+};
+
+signupButton.addEventListener("click", signUpAlert);
+
+
+
   
 
